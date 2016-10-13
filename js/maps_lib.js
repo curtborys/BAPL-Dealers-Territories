7b3d7c5dca9ba9c8948936d429d6c3b9341224fb
curtborys <curt.borys@gmail.com>
2016-10-13 14:14:57 -0600

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -166,10 +166,10 @@ var MapsLib = {
     if ( $("#cbType21").is(':checked')) searchType += "2,";    
         whereClause += " AND " + searchType.slice(0, searchType.length - 1) + ")";
     
-        var type_column = "'BAPL Sales'";
+    var type_column = "'BAPL Sales'";
     var searchType = type_column + " IN (-1,";
-    if ( $("#cbType22").is(':checked')) whereClause.push("No"); 
-        whereClause += " AND " + type_column + " IN ('" + whereClause.join('\',\' ') + "')";
+    if ( $("#cbType22").is(':checked')) searchType += "'No',"; 
+        whereClause += " AND " + searchType.slice(0, searchType.length - 1) + ")";
    
     //-------end of custom filters--------
 
