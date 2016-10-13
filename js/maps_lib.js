d8ffcd99f49f4e44f2b7bac3fa755d3d28193df5
curtborys <curt.borys@gmail.com>
2016-10-13 16:34:31 -0600

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -106,7 +106,6 @@ var MapsLib = {
 
     //-----custom initializers-------
       $("#rbPolygonOff").attr("checked", "checked");
-      $('#cbType22').removeAttr('checked');
     //-----end of custom initializers-------
 
     //run the default search
